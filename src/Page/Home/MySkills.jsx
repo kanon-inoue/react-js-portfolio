862e15ea8fffd40ac6ddc3ca35e345defdb633c2
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-11 20:39:41 +0900

    Skills style done

--- src/Page/Home/MySkills.jsx
+++ src/Page/Home/MySkills.jsx
@@ -2,14 +2,12 @@ import data from "../../data/index.json";
 
 export default function MySkills() {
   return (
-    <section className="skills--section"
-    id="mySkills">
+    <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
         <p className="section--title">My Skills</p>
-        <h2 className="skills--section--heading">
-          My Expertise</h2>
+        <h2 className="skills--section--heading">My Expertise</h2>
       </div>
-      <div className="skills--secion--container">
+      <div className="skills--section--container">
         {data?.skills?.map((item, index) => (
           <div key={index} className="skills--section--card">
             <div className="skills--section--img">
