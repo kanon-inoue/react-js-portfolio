cb35b672254a34142784c3feae3d5d1670c1aeda
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-11 19:12:17 +0900

    Navbar isx done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './Page/Home/Navbar';
 import Home from './Page/Home/Homescreen';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<div>404 Not Found</div>}></Route>
+      
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
