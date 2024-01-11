f59616ce9bce616e35d3e4c9c67d0e6c689522b1
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-11 20:02:58 +0900

    skills map done

--- src/Page/Home/MySkills.jsx
+++ src/Page/Home/MySkills.jsx
@@ -15,6 +15,10 @@ export default function MySkills() {
             <div className="skills--section--img">
               <img src={item.src} alt="Product Chain" />
             </div>
+            <div className="skills--section--card--content">
+              <h3 className="skills--section--title">{item.title}</h3>
+              <p className="skills--section--description">{item.description}</p>
+            </div>         
           </div>
         ))}
       </div>
