fbe93fb40232a914fd4b5802ad69f10e4bf8ac95
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-11 20:56:52 +0900

    about me style done

--- src/Page/Home/AboutMe.jsx
+++ src/Page/Home/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <p className="section-title">About</p>
+          <p className="section--title">About</p>
           <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section--description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officiis sit debitis omnis harum sed veniam quasi dicta accusamus recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam tempora numquam sint odit optio.
