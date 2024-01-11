9fc9a6aecec30651b9f79f0428410273864222cb
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-11 12:59:09 +0900

    basic setting done

---
+++ src/Page/Home/HeroSection.jsx
@@ -0,0 +1,22 @@
+export default function HeroSection () {
+  return <section id="heroSection" className="hero--section">
+      <div className="hero--section--comtent--box">
+        <div className="hero--section-content">
+          <p className="section--title">Hey, I'm Kanon</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <button className="btm btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/image1.jpeg" alt="Hero Section" />
+      </div>
+    </section>;
+}
